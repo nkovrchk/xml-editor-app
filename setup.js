12d0e716d4c2e112e1656d8e9ad5c67d8cf58757
nkovrchk <nkovrchk@gmail.com>
2021-09-29 18:05:25 +0300

    readme changes

--- setup.js
+++ setup.js
@@ -2,6 +2,8 @@ const { exec } = require('child_process');
 
 const SETUP = {
     darwin: 'sudo python3 -m venv "${PWD}/venv"\nsudo ./venv/bin/pip install -r requirements.txt\nnpm install',
+    win32: 'pip install -r requirements.txt\nnpm install',
+    linux: 'pip install -r requirements.txt\nnpm install',
 };
 
 exec(SETUP[process.platform], (err, stdout) => {
